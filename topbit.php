1f2171a989eb78f08acb9659b4b03135302f38b4
Rinkoo <63033059+Lindisfarne-RB@users.noreply.github.com>
2021-02-25 21:14:14 +1300

    Create topbit.php

---
+++ topbit.php
@@ -0,0 +1,58 @@
+Close Save View xzwvsige/topbit.php
+
+1
+<!DOCTYPE HTML>
+2
+​
+3
+<html lang="en">
+4
+​
+5
+<head>
+6
+  <meta charset="UTF-8">
+7
+  <meta name="description" content="add content">
+8
+  <meta name="keywords" content="adding test">
+9
+  <meta name="author" content="MAGS">
+10
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+11
+  <link rel="stylesheet" href="css/second.css">  
+12
+    
+13
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+14
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Permanent+Marker&display=swap" rel="stylesheet">
+15
+    
+16
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+17
+<link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet"> 
+18
+    
+19
+    <title> Home page </title>
+20
+</head>
+21
+    <body>
+22
+        <div class="wrapper">
+23
+            <header class="box"><img class=roundleft src="img/logobook.jpg" width=50 height=50 alt="book" />
+24
+                <h2>Welcome to Web development</h2> </header>
+25
+            
+26
+            <nav class=box> 
+27
+            
+28
+                <a class="dropbtn" href="index.php">Home </a>|
