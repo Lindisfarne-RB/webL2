2ad92790959359bb65302002012c728d81427c1d
Rinkoo <63033059+Lindisfarne-RB@users.noreply.github.com>
2021-02-25 21:11:42 +1300

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+			
+<?php include "topbit.php"; ?>
+<main class="box"> <h2> Home Page </h2>
+				<p>
+
+					Poetry is one of the things that makes us human.  Or is it?  </p>
+					<p>This site features poems on choose topic.  You are invited to read the poems and then try and work out if they were written by a human or if they were created with a poetry generator.
+				</p>
+  </main>
+			<aside class="box">
+				<h2> Generators </h2>
+				<p>
+If you’d like to generate some poems for yourself, here are some online generators that you 
+can use-</p>
+					<ul>
+						
+						<li><a href=https://www.poem-generator.org.uk> Poem Generator </a> </li>
+						<li> <a href=
+								http://www.languageisavirus.com/automatic_poetry_generator.php> auto </a> </li>
+				</ul>
+				
+				<p>
+					Know of a poetry generator not listed above?  <a href=contact.php> Please let me know </a> about it and I will add it to the list.</p>
+ </aside>
+			
+<?php include "bottombit.php"; ?>
